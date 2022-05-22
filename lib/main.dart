bf29b1d8cf03fc5e41a784f131e61a381d8aba86
Matheus Girardi <matheus.girardi@serpro.gov.br>
2022-05-21 22:25:36 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:payflow/modules/splash/splash_page.dart';
+import 'package:payflow/shared/themes/app_colors.dart';
+
+import 'modules/home/home_page.dart';
+import 'modules/login/login_page.dart';
+
+void main() {
+  runApp(const AppWidget());
+}
+
+class AppWidget extends StatelessWidget {
+  const AppWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Pay Flow',
+      theme: ThemeData(
+        primaryColor: AppColors.primary,
+      ),
+      home: const LoginPage(),
+    );
+  }
+}
