3c534bff9b61637cd9ed118de68bab5bf7131f7d
Matheus Girardi <matheus.girardi@serpro.gov.br>
2022-05-22 20:23:15 -0300

    add shared preferences and auth

--- lib/shared/auth/auth_controller.dart
+++ lib/shared/auth/auth_controller.dart
@@ -1,24 +1,37 @@
 import 'package:flutter/material.dart';
-import 'package:payflow/modules/home/home_page.dart';
-
-import '../../modules/login/login_page.dart';
+import 'package:payflow/shared/auth/models/user_model.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class AuthController {
-  var _isAuthencicated = false;
-  var _user;
+  UserModel? _user;
 
-  get user => _user;
+  UserModel get user => _user!;
 
-  void setUser(BuildContext context, var user) {
+  void setUser(BuildContext context, UserModel? user) {
     if (user != null) {
+      saveUser(user);
       _user = user;
-      _isAuthencicated = true;
-      Navigator.pushReplacementNamed(
-          context, "/home");
+      Navigator.pushReplacementNamed(context, "/home");
     } else {
-      _isAuthencicated = false;
-      Navigator.pushReplacementNamed(
-          context, "/login");
+      Navigator.pushReplacementNamed(context, "/login");
+    }
+  }
+
+  Future<void> saveUser(UserModel user) async {
+    final instance = await SharedPreferences.getInstance();
+    await instance.setString("user", user.toJson());
+    return;
+  }
+
+  Future<void> currentUser(BuildContext context) async {
+    final instance = await SharedPreferences.getInstance();
+    await Future.delayed(const Duration(seconds: 2));
+    if (instance.containsKey("user")) {
+      final json = instance.get("user") as String;
+      setUser(context, UserModel.fromJson(json));
+      return;
+    } else {
+      setUser(context, null);
     }
   }
 }
